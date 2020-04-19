350954047f62e5d7b288108ecdac8c49924fe945
Kounandi Ouattara <hellokounandi.codes>
2020-04-19 10:41:34 -0600

    project created and title added

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <Grid centered>
+      <Grid.Row>
+        <h1>To-Do List with React &amp; Redux</h1>
+      </Grid.Row>
+      <Grid.Row>
+      <h1>To-Do List Form</h1>
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default App;
